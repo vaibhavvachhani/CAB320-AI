4140afdfd7cb0232c2e6a0f233f106404fe07dd3
Nok Hei Heidi Cheng <nokheiheidi.cheng@uqconnect.edu.au>
2019-04-19 13:01:37 +1000

    correct search file

--- search.py
+++ search.py
@@ -12,9 +12,10 @@ The way to use this code is to subclass the class 'Problem' to create
 your own class of problems,  then create problem instances and solve them with 
 calls to the various search functions.
 
-Last modified 2019-03-10
+Last modified 2019-03-27
 by f.maire@qut.edu.au
-- Changed PriorityQueue 
+- Changed PriorityQueue , Node to aima-utils
+
 
 
 """
@@ -97,58 +98,118 @@ class FIFOQueue(collections.deque):
 
 
 import heapq
-class PriorityQueue(Queue):
-    """
-    A queue in which the minimum  element (as determined by f) is returned first.
-    The item with minimum f(x) is returned first
-    """
-    def __init__(self, f=lambda x: x):
-        self.heap = []  # list of pairs  (f(item), counter_value, item)
-        self.f = f
-        self.counter = itertools.count() # unique sequence count
-        # counter_value is used to break ties between items that
-        # have the same 'f' value
-        
+#class PriorityQueue(Queue):
+#    """
+#    A queue in which the minimum  element (as determined by f) is returned first.
+#    The item with minimum f(x) is returned first
+#    """
+#    def __init__(self, f=lambda x: x):
+#        self.heap = []  # list of pairs  (f(item), counter_value, item)
+#        self.f = f
+#        self.counter = itertools.count() # unique sequence count
+#        # counter_value is used to break ties between items that
+#        # have the same 'f' value
+#        
+#    def append(self, item):
+#        # thw pair  (f(item), counter_value, item)  is pushed on the internal heapq
+#        heapq.heappush(self.heap, (self.f(item),next(self.counter),  item))
+#
+#    def extend(self, items):
+#        """Insert each item in items at its correct position."""
+#        for item in items:
+#            self.append(item)
+#                
+#    def __len__(self):
+#        return len(self.heap)
+#    
+#    def __str__(self):
+#        return str(self.heap)
+#    
+#    def pop(self):
+#        """Pop and return the item with min f(x) value """
+#        if self.heap:
+#            # item is the last element of the tuple  (f(item), counter_value, item) 
+#            return heapq.heappop(self.heap)[-1]
+#        else:
+#            raise Exception('Trying to pop from empty PriorityQueue.')
+#    
+#    def __contains__(self, item):
+#        """Return True if item in PriorityQueue."""
+##        return (self.f(item), item) in self.heap
+#        for _,_, key in self.heap:
+#            # Note that two instances of 'Node' are considered
+#            # equal if their corresponding states are the same.
+#            if item == key:
+#                return True
+#        return False
+#    
+#
+#    def __getitem__(self, key):
+#        for _,_, item in self.heap:
+#            # Note that two instances of 'Node' are considered
+#            # equal if their corresponding states are the same.
+#            if item == key:
+#                return item
+#            
+#    def __delitem__(self, key):
+#        for f_value,count_value,item in self.heap:
+#            if item == key:
+#                self.heap.remove((f_value,count_value,item))
+#                heapq.heapify(self.heap)
+#                return
+class PriorityQueue(object): # from aima_utils 
+    """A Queue in which the minimum (or maximum) element (as determined by f and
+    order) is returned first.
+    If order is 'min', the item with minimum f(x) is
+    returned first; if order is 'max', then it is the item with maximum f(x).
+    Also supports dict-like lookup."""
+
+    def __init__(self,  f=lambda x: x, order='min'):
+        self.heap = []
+
+        if order == 'min':
+            self.f = f
+        elif order == 'max':  # now item with max f(x)
+            self.f = lambda x: -f(x)  # will be popped first
+        else:
+            raise ValueError("order must be either 'min' or 'max'.")
+
     def append(self, item):
-        # thw pair  (f(item), counter_value, item)  is pushed on the internal heapq
-        heapq.heappush(self.heap, (self.f(item),next(self.counter),  item))
+        """Insert item at its correct position."""
+        heapq.heappush(self.heap, (self.f(item), item))
 
     def extend(self, items):
         """Insert each item in items at its correct position."""
         for item in items:
             self.append(item)
-                
-    def __len__(self):
-        return len(self.heap)
-    
-    def __str__(self):
-        return str(self.heap)
-    
+
     def pop(self):
-        """Pop and return the item with min f(x) value """
+        """Pop and return the item (with min or max f(x) value)
+        depending on the order."""
         if self.heap:
-            # item is the last element of the tuple  (f(item), counter_value, item) 
-            return heapq.heappop(self.heap)[-1]
+            return heapq.heappop(self.heap)[1]
         else:
             raise Exception('Trying to pop from empty PriorityQueue.')
-    
+
+    def __len__(self):
+        """Return current capacity of PriorityQueue."""
+        return len(self.heap)
+
     def __contains__(self, item):
         """Return True if item in PriorityQueue."""
         return (self.f(item), item) in self.heap
 
     def __getitem__(self, key):
-        for _,_, item in self.heap:
-            # Note that two instances of 'Node' are considered
-            # equal if their corresponding states are the same.
+        for _, item in self.heap:
             if item == key:
                 return item
-            
+
     def __delitem__(self, key):
-        for f_value,count_value,item in self.heap:
-            if item == key:
-                self.heap.remove((f_value,count_value,item))
-                heapq.heapify(self.heap)
-                return
+        """Delete the first occurrence of key."""
+        self.heap.remove((self.f(key), key))
+        heapq.heapify(self.heap)
+
+
 
 #______________________________________________________________________________
 
@@ -197,64 +258,129 @@ class Problem(object):
         raise NotImplementedError
 #______________________________________________________________________________
 
-class Node:
-    """
-    A node in a search tree. Contains a pointer to the parent (the node
+class Node: # aima_utils
+
+    """A node in a search tree. Contains a pointer to the parent (the node
     that this is a successor of) and to the actual state for this node. Note
     that if a state is arrived at by two paths, then there are two nodes with
     the same state.  Also includes the action that got us to this state, and
     the total path_cost (also known as g) to reach the node.  Other functions
     may add an f and h value; see best_first_graph_search and astar_search for
     an explanation of how the f and h values are handled. You will not need to
-    subclass this class.
-    """
+    subclass this class."""
 
     def __init__(self, state, parent=None, action=None, path_cost=0):
-        "Create a search tree Node, derived from a parent by an action."
-        update(self, state=state, parent=parent, action=action,
-               path_cost=path_cost, depth=0)
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
         if parent:
             self.depth = parent.depth + 1
 
     def __repr__(self):
-        return "<Node %s>" % (self.state,)
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
 
     def expand(self, problem):
-        "List the nodes reachable in one step from this node."
+        """List the nodes reachable in one step from this node."""
         return [self.child_node(problem, action)
                 for action in problem.actions(self.state)]
 
     def child_node(self, problem, action):
-        "Fig. 3.10"
+        """[Figure 3.10]"""
         next_state = problem.result(self.state, action)
-        return Node(next_state, # next_state is a state
-                    self, # parent is a node
-                    action, # from this state to next state 
-                    problem.path_cost(self.path_cost, self.state, action, next_state)
-                    )
-
+        next_node = Node(next_state, self, action,
+                    problem.path_cost(self.path_cost, self.state,
+                                      action, next_state))
+        return next_node
+    
     def solution(self):
-        "Return the sequence of actions to go from the root to this node."
+        """Return the sequence of actions to go from the root to this node."""
         return [node.action for node in self.path()[1:]]
 
     def path(self):
-        "Return a list of nodes forming the path from the root to this node."
+        """Return a list of nodes forming the path from the root to this node."""
         node, path_back = self, []
         while node:
             path_back.append(node)
             node = node.parent
         return list(reversed(path_back))
 
-    # We want for a queue of nodes in breadth_first_search or
+    # We want for a queue of nodes in breadth_first_graph_search or
     # astar_search to have no duplicated states, so we treat nodes
     # with the same state as equal. [Problem: this may not be what you
     # want in other contexts.]
+
     def __eq__(self, other):
         return isinstance(other, Node) and self.state == other.state
 
     def __hash__(self):
         return hash(self.state)
 
+
+#
+#class Node:
+#    """
+#    A node in a search tree. Contains a pointer to the parent (the node
+#    that this is a successor of) and to the actual state for this node. Note
+#    that if a state is arrived at by two paths, then there are two nodes with
+#    the same state.  Also includes the action that got us to this state, and
+#    the total path_cost (also known as g) to reach the node.  Other functions
+#    may add an f and h value; see best_first_graph_search and astar_search for
+#    an explanation of how the f and h values are handled. You will not need to
+#    subclass this class.
+#    """
+#
+#    def __init__(self, state, parent=None, action=None, path_cost=0):
+#        "Create a search tree Node, derived from a parent by an action."
+#        update(self, state=state, parent=parent, action=action,
+#               path_cost=path_cost, depth=0)
+#        if parent:
+#            self.depth = parent.depth + 1
+#
+#    def __repr__(self):
+#        return "<Node %s>" % (self.state,)
+#
+#    def expand(self, problem):
+#        "List the nodes reachable in one step from this node."
+#        return [self.child_node(problem, action)
+#                for action in problem.actions(self.state)]
+#
+#    def child_node(self, problem, action):
+#        "Fig. 3.10"
+#        next_state = problem.result(self.state, action)
+#        return Node(next_state, # next_state is a state
+#                    self, # parent is a node
+#                    action, # from this state to next state 
+#                    problem.path_cost(self.path_cost, self.state, action, next_state)
+#                    )
+#
+#    def solution(self):
+#        "Return the sequence of actions to go from the root to this node."
+#        return [node.action for node in self.path()[1:]]
+#
+#    def path(self):
+#        "Return a list of nodes forming the path from the root to this node."
+#        node, path_back = self, []
+#        while node:
+#            path_back.append(node)
+#            node = node.parent
+#        return list(reversed(path_back))
+#
+#    # We want for a queue of nodes in breadth_first_search or
+#    # astar_search to have no duplicated states, so we treat nodes
+#    # with the same state as equal. [Problem: this may not be what you
+#    # want in other contexts.]
+#    def __eq__(self, other):
+#        return isinstance(other, Node) and self.state == other.state
+#
+#    def __hash__(self):
+#        return hash(self.state)
+#
 #______________________________________________________________________________
 
 # Uninformed Search algorithms
@@ -422,7 +548,8 @@ def astar_graph_search(problem, h=None):
     """A* search is best-first graph search with f(n) = g(n)+h(n).
     You need to specify the h function when you call astar_search, or
     else in your Problem subclass."""
-    h = memoize(h or problem.h)
+#    h = memoize(h or problem.h)
+    h = h or problem.h
     return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
 
 

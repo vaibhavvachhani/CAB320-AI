040d4d52cf1266229dd0afa4eb082dd288dd9ef6
Dazmi <36841434+Dazmi@users.noreply.github.com>
2019-04-19 19:13:49 +1000

    updated testing checker

--- mychecker.py
+++ mychecker.py
@@ -20,16 +20,37 @@ from mySokobanSolver import solve_sokoban_elem, can_go_there, solve_sokoban_macr
 #from fredSokobanSolver import my_team, taboo_cells, SokobanPuzzle, check_action_seq
 #from fredSokobanSolver import solve_sokoban_elem, can_go_there, solve_sokoban_macro 
 
-
 import time
-t0 = time.time()    
-wh = Warehouse()
-wh.load_warehouse("./warehouses/warehouse_11.txt")
-answer = solve_sokoban_elem(wh)
-t1 = time.time()
-print(wh)
-print(answer)
-print ('A* Solver took {:.6f} seconds\n'.format(t1-t0))
+if __name__ == "__main__":
+
+    def tester(num):
+        problem_file = "./warehouses/warehouse_{:02d}.txt".format(num)
+
+        wh = Warehouse()
+        wh.load_warehouse(problem_file)
+
+        t0 = time.time()    
+        result = solve_sokoban_elem(wh)
+        t1 = time.time()
+        print ('Warehouse number:{0}'.format(num))
+        print ('Uniform Solver elem took {:.6f} seconds'.format(t1-t0))
+
+        wh = Warehouse()
+        wh.load_warehouse(problem_file)
+
+        t0 = time.time()    
+        result = solve_sokoban_elem(wh)
+        t1 = time.time()
+        print ('Uniform Solver marco took {:.6f} seconds\n'.format(t1-t0))
+
+
+    for num in [1, 3, 5, 9, 11, 15]:
+        tester(num)
+
+    for num in range(15, 100, 2):
+        tester(num)
+
+        
 
         
 
